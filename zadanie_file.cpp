21a03268609af9ba43d367a13492fdbb03b1d711
mensatoris <vmensator@gmail.com>
2017-11-08 21:01:08 +0100

    všetko OK podľa zadania aj s Bonus, vylepšenie je dalej možné refaktoring

--- zadanie_file.cpp
+++ zadanie_file.cpp
@@ -1,4 +1,10 @@
 
+/*
+	Denisa Mensatorisová
+	Zadanie ZS 2017
+	3.11.2017
+*/
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "time.h"
@@ -6,7 +12,6 @@
 
 FILE *vypis(FILE *fr)
 {
-  //FILE *fr;
   int z;
   char popis1[] = "meno priezvisko: ";
   char popis2[] = "SPZ: ";
@@ -88,7 +93,6 @@ FILE *vypis(FILE *fr)
 
 void odmena(FILE *sub, char *sdat)
 {
-
 	int  x, t;
 	float cena2; float odm = 0;
 	float perc0 = 0.022, perc1 = 0.015;
@@ -149,11 +153,13 @@ char *vytvorpole(FILE *sub, char *pole)
 	int x, i ;   
 	int n = 0;           /* pocet zaznamov */
 	int velkost = 0;     /* velkost pola SPZ */
-	char meno[10]; char priez[16]; 
-	char spz[7]; char typ[2]; char cena[9]; char datum[8]; 
+	char meno[10]; char priez[16]; char spz[7]; char typ[2]; char cena[9]; char datum[8]; 
 	char *pom;	   /* pre ulozenie zaciatok pola */
+	FILE *p_fw;
 
 	pom = NULL;
+	p_fw = NULL;
+	
 	if (sub != NULL)
 	{
 	    do
@@ -178,12 +184,12 @@ char *vytvorpole(FILE *sub, char *pole)
 		} while (true);
 		
 		
-		printf("pocet zaznamov: %d\n", n);
-		printf("velkost pola SPZ: %d\n", velkost);	/* len na test vypisy */
+		//printf("pocet zaznamov: %d\n", n);
+		//printf("velkost pola SPZ: %d\n", velkost);	/* len na test vypisy */
 			
-		rewind(sub);
+		rewind(sub);      // smernik chod na zaciatok suboru
 	
-		printf("adresa pole pred: %p\n", pole);		/* len na test */
+		//printf("adresa pole pred: %p\n", pole);		/* len na test */
 		
 		if (pole == NULL)
 		{
@@ -208,7 +214,7 @@ char *vytvorpole(FILE *sub, char *pole)
 			pom = pole;
 		}
 		
-		printf("adresa pole po alokacii: %p\n", pole); 		/* len na test */
+		//printf("adresa pole po alokacii: %p\n", pole); 		/* len na test */
 
 		/* teraz naplnim pole SPZ */
 		n = 0;
@@ -232,11 +238,26 @@ char *vytvorpole(FILE *sub, char *pole)
 		} while (true);
 		
 		pole = pom;			/* nastav adresu smernika pole na zaciatok */
-		rewind(sub);
+		rewind(sub);        // smernik na subor na zaciatok 
 		
-		/*
+		
+		// potrebujeme este ulozit aj pre ine funkcie pocet zaznamov v subore
+		
+		p_fw = fopen(".\\poczaznamov.txt", "w");
+		if (p_fw != NULL)
+		{
+			fprintf(p_fw,"%d\n",n);
+			fflush(p_fw);
+			fclose(p_fw);
+		}
+		else
+		{
+			printf("chyba pri zapise do suboru\n");
+		}
+				
 		//printf("adresa pole po naplneni a nastaveni na zaciatok: %p\n", pole); 		// len na test 
 		
+		/*
 		printf("obsah pola :\n");
 		for (i = 0; i<n*velkost;i++)
 		{ 
@@ -244,8 +265,9 @@ char *vytvorpole(FILE *sub, char *pole)
 			pole++;
 		}
 		printf("\n");
+		*/
 		pole = pom;		// nastav adresu smernika pole na zaciatok
-		*/ 
+		 
 	}
 		
 	return pole;
@@ -255,23 +277,37 @@ char *vytvorpole(FILE *sub, char *pole)
 void vypisSPZ(char *pole)
 {
 	char *pom;	   /* pre ulozenie zaciatok pola */
-	int i = 0; int j;
+	int i = 0; int j; int n = 0;
+	FILE *p_fr;
 	
 	pom = NULL;
-	
+	p_fr = NULL;
+		
 	if (pole == NULL)
 	{
 		printf("Pole nie je vytvorene\n");
 	}
 	else
 	{
-		pom = pole;
-		for (i=0; i< 10 ; i++)
+		// zistim najprv pocet zaznamov ulozeny v subore
+		p_fr = fopen(".\\poczaznamov.txt", "r");
+		if (p_fr != NULL)
 		{
-			for (j=0;j<7;j++)
+			fscanf(p_fr,"%d",&n);
+			fclose(p_fr);
+		}
+		else
+		{
+			printf("chyba pri citani zo suboru\n");
+		}
+
+		pom = pole;
+		for (i=0; i < n ; i++)
+		{
+			for (j=0; j < 7; j++)      // 7 je dlzka SPZ
 			{
 				printf("%c", *pole);
-				if((j==1) || (j == 4)) printf(" ");		/* vloz medzeru */
+				if((j == 1) || (j == 4)) printf(" ");		/* vloz medzeru */
 				pole++;	
 			}
 			printf("\n");
@@ -284,9 +320,11 @@ void vypisSPZ(char *pole)
 void vypisPalindrom(char *pole)
 {
 	char *pom;	   /* pre ulozenie zaciatok pola */
-	int i = 0; int j;
+	int i = 0; int n = 0;
+	FILE *p_fr;	
 	
 	pom = NULL;
+	p_fr = NULL;
 	
 	if (pole == NULL)
 	{
@@ -294,10 +332,22 @@ void vypisPalindrom(char *pole)
 	}
 	else
 	{
-		pom = pole;
-		for (i=0; i< 10 ; i++)
+		// zistim najprv pocet zaznamov ulozeny v subore
+		p_fr = fopen(".\\poczaznamov.txt", "r");
+		if (p_fr != NULL)
 		{
-			if( (*pole == *(pole+6)) && (*(pole+1) == *(pole+5)) )
+			fscanf(p_fr,"%d",&n);
+			fclose(p_fr);
+		}
+		else
+		{
+			printf("chyba pri citani zo suboru\n");
+		}
+				
+		pom = pole;
+		for (i=0; i < n ; i++)
+		{
+			if( (*pole == *(pole+6)) && (*(pole+1) == *(pole+5)) && (*(pole+2) == *(pole+4)) )
 			{
 				printf("%c%c\n", *pole, *(pole+1));   // OK
 				//printf("%c%c%c%c\n", *pole, *(pole+1), *(pole+2), *(pole+3));   // test
@@ -311,6 +361,91 @@ void vypisPalindrom(char *pole)
 		pole = pom;
 	}	
 }
+
+
+
+void Bonus(char *pole)
+{
+	char *pom; char *pole2;	   /* pre ulozenie zaciatok pola */
+	int i = 0; int j = 0; int n = 0;
+	FILE *p_fr;	
+	
+	pom = NULL; pole2 = NULL; p_fr = NULL;
+	
+	if (pole == NULL)
+	{
+		printf("Pole nie je vytvorene\n");
+	}
+	else
+	{
+		// zistim najprv pocet zaznamov ulozeny v subore
+		p_fr = fopen(".\\poczaznamov.txt", "r");
+		if (p_fr != NULL)
+		{
+			fscanf(p_fr,"%d",&n);
+			fclose(p_fr);
+		}
+		else
+		{
+			printf("chyba pri citani zo suboru\n");
+		}
+						
+		int pocet[n]; int maxpocet = 1;
+		
+		for (i = 0; i < n; i++) 
+		{
+			pocet[i] = 1;
+		}
+		
+		pom = pole;
+		pole2 = pole+7;  // pole2 bude sluzit na krokovanie v dalsom cykle
+		
+		for (i = 0; i < (n-1) ; i++)          // posun pole
+		{			
+		    //printf("%d %c%c\n", i, *pole, *(pole+1));    // len na test
+		    
+			for( j = (i+1); j < n; j++ )      // posun pole2 
+			{
+			    //printf("  %d %c%c\n", j, *pole2, *(pole2+1));    // len na test
+			    
+				if( (*pole == *pole2) && (*(pole+1) == *(pole2+1)) )
+				{
+					pocet[i] = (pocet[i] + 1);    // ak je zhoda tak zvys pocet pre i-ty prvok-okres
+					if (pocet[i] > maxpocet) 
+					{
+						maxpocet = pocet[i];      // ak je viac ako maxpocet tak aktualizuj maxpocet
+					}
+				}
+				pole2 = pole2 + 7;    // skoc na dalsiu SPZ
+			}
+			pole = pole + 7;    // skoc na dalsiu SPZ
+			pole2 = pole + 7;   // skoc na dalsiu SPZ ale o 1 vyssiu ako ukazuje pole
+		}
+		pole = pom;
+		pole2= pom;
+
+		/*
+		printf("adresa pole bonus: %p\n", pole); 		// len na test 
+
+		for (i = 0; i < n; i++) 
+		{
+			printf("pocet %d : %d\n", i, pocet[i] );    // len na test 
+		}
+		*/
+		
+		for (i = 0; i < n ; i++)         // vypis vysledok - okres s najviac predanych aut
+		{
+			if(pocet[i] == maxpocet)
+			{ 
+				printf("%c%c %d\n", *pole, *(pole + 1), pocet[i]);    // OK - ak by bolo viac s maxpocet tak vypise vsetky s maxpocet
+			}
+			pole = pole+7;   // skoc na dalsiu SPZ
+		}
+		pole = pom;
+	}	
+	
+}
+
 
 
 int main()
@@ -375,9 +510,9 @@ do
 		}
 		else {strcat(sdatum,sden);}
 
-		//printf("%s\n", sdatum);      // len test ci to robi dobre
+		//printf("%s\n", sdatum);        // len test ci to robi dobre
 				
-		if (c == 'v')    // iba ak bola volba "v"
+		if (c == 'v')     // iba ak bola volba "v"
 		{
 			odmena(ff, sdatum);
 		}
@@ -400,11 +535,12 @@ do
 		}
 	case 'p' :
 		{
-			vypisPalindrom(p_pole);
+			vypisPalindrom(p_pole);   // OK
 			break;
 		}
 	case 'z' :
 		{
+			Bonus(p_pole);     // OK
 			break;
 		}
     default: continue;
@@ -417,9 +553,9 @@ if (c == 'v')
 	if (ff != NULL) fclose(ff);
 }
 
-//if (ff != NULL) free(ff);              // toto vykazuje nejaku chybu ??!!!
+//if (ff != NULL) free(ff);              // pri tomto vykazuje chybu ??!!! - neviem preèo nechce uvo¾ni pamä
 ff = NULL;
-//if (p_pole != NULL) free(p_pole);      // aj toto vykazuje nejaku chybu !??!!
+//if (p_pole != NULL) free(p_pole);      // aj pri tomto vykazuje chybu !??!!
 p_pole = NULL;
 
 return 0;
